6738683881573315d5515d9c62202ea708c9b355
melba raj <smmelbaraj@gmail.com>
2020-12-22 11:31:37 +0530

    Question 1.4 commit

---
+++ src/main/scala/CombinedReadLineAndWriteChar.scala
@@ -0,0 +1,52 @@
+import java.io.File
+
+import Streams.{InputStream, OutputStream}
+
+object CombinedReadingAndWriting extends App {
+
+  val folderPath: String="src/main/resources/sampleFiles/"
+  val file1 = new File(folderPath)
+  var strBuff:StringBuffer =null
+  rrMerge(2)
+
+  def rrMerge(f:Int):Unit={
+
+       if(file1.exists() && file1.isDirectory){
+         val fileArray: Array[String] = file1.listFiles(_.isFile).
+                                        filter(_.getName.endsWith(".csv")).
+                                        map(i=>folderPath + i.getName)
+
+         var inputArray: Array[InputStream] = new Array[InputStream](f)
+
+         val outputStream = new OutputStream("src/main/resources/readWriteFile.csv")
+         outputStream.create()
+
+        for(i<- 0 to f-1) {
+          val fName = new File(fileArray(i))
+          inputArray(i) = new InputStream(fName)
+          inputArray(i).open()
+        }
+
+        var while_var = true
+        while (while_var) {
+          for (i <- 0 to f - 1) {
+              strBuff = inputArray(i).readLine()
+//              strBuff = inputArray(i).readCharacter()
+//              strBuff = inputArray(i).readCharacterWithBuffer(5)
+              if(!inputArray(i).endOfStream) {
+                //outputStream.writeLine(strBuff.toString) //works
+               outputStream.writeCharacter(strBuff)
+              }
+              while_var = while_var & inputArray(i).endOfStream
+          }
+          while_var = !while_var
+        }
+         for(i<- 0 to f-1) {
+           inputArray(i).close()
+        }
+         outputStream.close
+       }
+  }
+
+
+}
